fe9de1b0f73840189caba8370212ff655d34c3bb
涂松高 <tusonggao@163.com>
2018-11-23 22:51:54 +0800

    add models

--- portfolio/gallery/admin.py
+++ portfolio/gallery/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Gallery
+
+import pandas as pd
 
 # Register your models here.
+
+admin.site.register(Gallery)
+
+
--- portfolio/gallery/models.py
+++ portfolio/gallery/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+import pandas as pd
 # Create your models here.
+
+class Gallery(models.Model):
+	description = models.CharField(max_length=100)
+    
