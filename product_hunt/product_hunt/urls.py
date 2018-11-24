6a38c4ea995cb1508068abec11b7902c8426b21a
涂松高 <tusonggao@163.com>
2018-11-24 15:54:55 +0800

    add templates files

--- product_hunt/product_hunt/urls.py
+++ product_hunt/product_hunt/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import products.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', products.views.product_list),
 ]
