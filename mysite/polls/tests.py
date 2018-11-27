8e9cbee6e83994436ffd88e1f44012a160a0c3a3
涂松高 <tusonggao@163.com>
2018-11-27 17:52:45 +0800

    hot fix for test

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -36,6 +36,35 @@ class QuestionDetailViewTests(TestCase):
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
 
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+	    """
+	    was_published_recently() returns False for questions whose pub_date
+	    is older than 1 day.
+	    """
+	    time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+	    old_question = Question(pub_date=time)
+	    self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+	    """
+	    was_published_recently() returns True for questions whose pub_date
+	    is within the last day.
+	    """
+	    time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+	    recent_question = Question(pub_date=time)
+	    self.assertIs(recent_question.was_published_recently(), True)
+
 
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
@@ -94,33 +123,3 @@ class QuestionIndexViewTests(TestCase):
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
 
-
-
-class QuestionModelTests(TestCase):
-
-    def test_was_published_recently_with_future_question(self):
-        """
-        was_published_recently() returns False for questions whose pub_date
-        is in the future.
-        """
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
-        self.assertIs(future_question.was_published_recently(), False)
-
-    def test_was_published_recently_with_old_question(self):
-	    """
-	    was_published_recently() returns False for questions whose pub_date
-	    is older than 1 day.
-	    """
-	    time = timezone.now() - datetime.timedelta(days=1, seconds=1)
-	    old_question = Question(pub_date=time)
-	    self.assertIs(old_question.was_published_recently(), False)
-
-    def test_was_published_recently_with_recent_question(self):
-	    """
-	    was_published_recently() returns True for questions whose pub_date
-	    is within the last day.
-	    """
-	    time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
-	    recent_question = Question(pub_date=time)
-	    self.assertIs(recent_question.was_published_recently(), True)
