09c1fb94f756f412089ee2f4ccb60e82b04f930a
涂松高 <tusonggao@163.com>
2018-11-28 11:17:08 +0800

    change admin settings

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,5 +4,30 @@ from django.contrib import admin
 from .models import Question
 from .models import Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 4
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+# admin.site.register(Question, QuestionAdmin)
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None,               {'fields': ['question_text']}),
+#         ('Date information', {'fields': ['pub_date']}),
+#     ]
+
+# admin.site.register(Choice)
+# admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
