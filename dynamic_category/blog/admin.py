5e61499a816884df263b8f04dc96c0b7962d327a
tusonggao <tusonggao@163.com>
2019-01-02 07:32:55 +0800

    update file

--- dynamic_category/blog/admin.py
+++ dynamic_category/blog/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-
+from django.conf.urls import include
 # Register your models here.
-
 from .models import Category
 
 # Register your models here.
 
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',)}
+    prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Category, CategoryAdmin)
