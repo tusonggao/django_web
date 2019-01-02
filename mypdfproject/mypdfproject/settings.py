30df9d41c6f8d0c0ac04c4ef190b9ebdaf02a1d0
tusonggao <tusonggao@163.com>
2019-01-02 14:04:06 +0800

    update file

--- mypdfproject/mypdfproject/settings.py
+++ mypdfproject/mypdfproject/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# 设置媒体文件夹, 对于图片和文件上传很重要
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- mypdfproject/mypdfproject/urls.py
+++ mypdfproject/mypdfproject/urls.py
@@ -16,6 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from django.contrib import admin
+from django.urls import path, include
+
+# 对于显示静态文件非常重要
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('pdf/', include('pdf.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
