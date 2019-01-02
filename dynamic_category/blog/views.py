7ab39089986f68abc2de852206cc6d1426268aa2
tusonggao <tusonggao@163.com>
2019-01-02 11:35:07 +0800

    update views.py

--- dynamic_category/blog/views.py
+++ dynamic_category/blog/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse(u'Hello World')
+    return HttpResponse(u'Hello, Django')
 
 class CategoryListView(ListView):
     model = Category
