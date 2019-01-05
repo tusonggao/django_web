55a9b187797761dae5a76328253d5aeec6512c42
tusonggao <tusonggao@163.com>
2019-01-06 00:12:42 +0800

    udpate file

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from todo.views import todoView
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('todo/', todoView),
 ]
--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def todoView(request):
+    # return HttpResponse('hello, this is todoView.')
+    return render(request, 'todo.html')
